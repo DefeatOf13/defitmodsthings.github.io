9e07b123f084bf30cd67269b0520f41201c6c25e
Defit <DefeatOf13@gmail.com>
2024-09-24 15:24:06 +0200

    First commit

---
+++ src/data/constants.ts
@@ -0,0 +1,26 @@
+export const author = "DefeatOf13";
+
+export const defaultMeta = {
+  title: "DefitModsThings",
+  description:
+    "Personal blog of DefeatOf13",
+  ogImage: {
+    src: "/static/banner.png",
+    alt: "Astro Zinc with Astro Logo",
+  },
+};
+
+export const navigation = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/blog/",
+    text: "Blog",
+  },
+  {
+    href: "/rss.xml",
+    text: "RSS",
+  },
+];
